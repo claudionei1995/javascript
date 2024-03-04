8d59612fb6ffad9fdbc5e5129eba4eea08542195
Claudionei1995 <claudionei0580@gmail.com>
2024-03-04 17:57:57 -0300

    javascript-treinando

---
+++ script.js
@@ -0,0 +1,17 @@
+function cliqueNoBotao() {
+    const input = document.getElementById("main-input");
+    console.log(input);
+}
+    
+function apertaraoaqui(){
+
+alert("fui acionado")
+
+}
+function digiteinoinput(){
+console.log("digitei no input")
+
+
+}
+
+
